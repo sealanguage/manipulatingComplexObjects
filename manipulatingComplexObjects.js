a60573230c26348bd7ae8e51cee64b046ac018a2
Elaine Froehlich <elaine.froehlich@gmail.com>
2017-12-16 01:17:42 +0000

    initial commit

---
+++ manipulatingComplexObjects.js
@@ -0,0 +1,26 @@
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  },
+  {
+    "artist": "Daft Punk",
+    "title": "Homework",
+    "release_year": 1997,
+     "formats": [
+       "CD",
+       "Cassette",
+       "LP"
+     ],
+      "gold": "true"
+  }
+  // Add record here
+];
+
